a772404c589c9ff68a8430bd8bde9bbe38c63a78
DarrenVs <DarrenVanstaen@Gmail.com>
2015-11-25 14:48:53 +0100

    UI Layout finished

---
+++ Assets/Scripts/UIActions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIActions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeScene(int sceneNumber)
+    {
+        Application.LoadLevel(sceneNumber);
+    }
+
+    public void PauseGame()
+    {
+        Time.fixedDeltaTime = 0;
+    }
+}
